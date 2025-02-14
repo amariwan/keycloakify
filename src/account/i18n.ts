efe2e339f57d8227788657fbe969c1b6563ee9ee
Aland Baban <amariwan9@icloud.com>
2025-02-14 14:53:44 +0000

    Entferne nicht verwendete E-Mail-Vorlagen und -Layouts zur Codebereinigung; exportiere i18n-Objekt in der i18n-Konfiguration

--- src/account/i18n.ts
+++ src/account/i18n.ts
@@ -5,4 +5,6 @@ const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
 
 type I18n = typeof ofTypeI18n;
 
-export { useI18n, type I18n };
+const i18n = ofTypeI18n; // Fügen Sie diese Zeile hinzu
+
+export { useI18n, i18n, type I18n }; // Exportieren Sie i18n
