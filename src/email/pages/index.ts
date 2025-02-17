518c21b6421779f8c503ef1f60a28b9273e9cd1d
Aland Baban <amariwan9@icloud.com>
2025-02-17 02:00:21 +0000

    Entferne nicht verwendete E-Mail-Komponenten und -Dateien zur Codebereinigung

--- src/email/pages/index.ts
+++
@@ -1,8 +0,0 @@
-import { lazy } from "react";
-export const pages: Record<
-  string,
-  React.LazyExoticComponent<React.ComponentType<any>>
-> = {
-  "email-test.ftl": lazy(() => import("./emailTest")),
-  "email-verify.ftl": lazy(() => import("./VerifyEmail"))
-};
