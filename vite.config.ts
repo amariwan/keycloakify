474accce597ec6a21e21c7f475628c7ead2d2a82
Aland Baban <amariwan9@icloud.com>
2025-02-17 02:09:29 +0000

    Aktualisiere Vite-Konfiguration: Entferne nicht verwendete E-Mail-Theme-Implementierung und bereinige den Code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
-import { buildEmailTheme } from "keycloakify-emails";
 import * as path from "node:path";
 
 export default defineConfig({
@@ -9,24 +8,13 @@ export default defineConfig({
   plugins: [
     react(),
     keycloakify({
-      accountThemeImplementation: "Single-Page",
+      accountThemeImplementation: "none",
       keycloakVersionTargets: {
         "all-other-versions": "reha-inside-theme.jar",
         "22-to-25": false
       },
       themeName: "reha-inside-theme",
-      themeVersion: "1.0.0",
-      postBuild: async buildContext => {
-        await buildEmailTheme({
-          templatesSrcDirPath: import.meta.dirname + "/emails/page",
-          themeNames: buildContext.themeNames,
-          keycloakifyBuildDirPath: buildContext.keycloakifyBuildDirPath,
-          locales: ["en", "de"],
-          i18nSourceFile: import.meta.dirname + "/emails/i18n.ts",
-          cwd: import.meta.dirname,
-          esbuild: {}
-        });
-      }
+      themeVersion: "1.0.0"
     })
   ],
   resolve: {
